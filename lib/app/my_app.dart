1401c7920d8dd23686c08fc251ac2cfdf0a7db84
Ignacio98 <dani.manchu1998@gmail.com>
2024-10-29 06:19:23 -0500

    Adding home screen.

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_flutter/app/screens/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Widgets by Flutter',
+      home: HomeScreen(),
     );
   }
 }
