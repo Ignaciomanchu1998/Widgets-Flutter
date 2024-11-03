948d1bc27c76d8e49d54cdac954756d8ae030654
Ignacio98 <dani.manchu1998@gmail.com>
2024-11-03 17:02:48 -0500

    Agregando la opción de copiar código.

--- lib/app/presentation/shared/code_screen_shared.dart
+++ lib/app/presentation/shared/code_screen_shared.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_code_editor/flutter_code_editor.dart';
 import 'package:widgets_flutter/app/presentation/presentation.dart';
 import 'package:widgets_flutter/config/theme/code_viewer_theme.dart';
@@ -27,21 +28,36 @@ class CodeScreenShared extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: CodeTheme(
-          data: CodeThemeData(styles: monokaiSublimeTheme),
-          child: CodeField(
-            textStyle: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-            controller: CodeController(text: code, language: dart),
-            gutterStyle: const GutterStyle(
-              showErrors: true,
-              showFoldingHandles: false,
-              showLineNumbers: false,
+        child: SingleChildScrollView(
+          child: CodeTheme(
+            data: CodeThemeData(styles: monokaiSublimeTheme),
+            child: CodeField(
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+              controller: CodeController(text: code, language: dart),
+              gutterStyle: const GutterStyle(
+                showErrors: true,
+                showFoldingHandles: false,
+                showLineNumbers: false,
+              ),
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Clipboard.setData(ClipboardData(text: code));
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: TextCustomWidget(
+                text: 'Código copiado al portapapeles',
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.copy_outlined),
       ),
     );
   }
